c153134660c06fbf9bd045c2d80408d4de91a9ae
carloosarias <carloosarias@gmail.com>
2018-05-21 14:43:09 -0500

    Commit

--- src/msa_ms/MainApp.java
+++ src/msa_ms/MainApp.java
@@ -1,53 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package msa_ms;
 
-import com.mysql.jdbc.Connection;
+import dao.DAOFactory;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  *
  * @author Pavilion Mini
  */
-public class MainApp extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                Connection con = ConnectionFactory.getConnection();
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class MainApp extends Application{
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
