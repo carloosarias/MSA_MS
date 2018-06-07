61f0538b4b91569110ec74cbc2891ab4e6f5bc2e
carloosarias <carloosarias@gmail.com>
2018-06-07 15:21:31 -0500

    Commit

--- src/controller/ProductPartFX.java
+++ src/controller/ProductPartFX.java
@@ -5,9 +5,16 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -16,12 +23,21 @@ import javafx.fxml.Initializable;
  */
 public class ProductPartFX implements Initializable {
 
+    @FXML
+    private ComboBox<?> filter_combo;
+    @FXML
+    private HBox revision_hbox;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            revision_hbox.getChildren().setAll(((HBox) FXMLLoader.load(getClass().getResource("/fxml/PartRevisionFX.fxml"))).getChildren());
+                    } catch (IOException ex) {
+            Logger.getLogger(ProductPartFX.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
