68cdaaed60ffb01957483786824cd247f046f32d
carloosarias <carloosarias@gmail.com>
2018-06-12 13:21:28 -0500

    Commit

--- src/controller/SpecificationFX.java
+++ src/controller/SpecificationFX.java
@@ -7,7 +7,15 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.Specification;
 
 /**
  * FXML Controller class
@@ -15,6 +23,27 @@ import javafx.fxml.Initializable;
  * @author Pavilion Mini
  */
 public class SpecificationFX implements Initializable {
+
+    @FXML
+    private ComboBox<String> filter_combo;
+    @FXML
+    private ListView<Specification> specification_listview;
+    @FXML
+    private Button add_button;
+    @FXML
+    private TextField id_field;
+    @FXML
+    private TextField specificationnumber_field;
+    @FXML
+    private TextArea details_field;
+    @FXML
+    private CheckBox active_check;
+    @FXML
+    private Button edit_button;
+    @FXML
+    private Button save_button;
+    @FXML
+    private Button cancel_button;
 
     /**
      * Initializes the controller class.
