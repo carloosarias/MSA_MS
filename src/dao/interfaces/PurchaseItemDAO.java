7557df4b8796c2556d682a0350c8fe1ffeb81d73
carloosarias <carloosarias@gmail.com>
2018-06-15 11:58:38 -0500

    Commit

--- src/dao/interfaces/PurchaseItemDAO.java
+++ src/dao/interfaces/PurchaseItemDAO.java
@@ -24,12 +24,50 @@ public interface PurchaseItemDAO {
      */    
     public PurchaseItem find(Integer id) throws DAOException;
     
+    /**
+     * Returns the OrderPurchase from the database matching the given PurchaseItem ID, otherwise null.
+     * PurchaseItem ID must not be null, otherwise it will throw IllegalArgumentException.
+     * @param purchase_item The PurchaseItem ID to get the OrderPurchase from.
+     * @return The OrderPurchase from the database matching the given PurchaseItem ID, otherwise null.
+     * @throws IllegalArgumentException If PurchaseItem ID is null.
+     * @throws DAOException If something fails at database level.
+     */      
     public OrderPurchase findOrderPurchase(PurchaseItem purchase_item) throws IllegalArgumentException, DAOException;
     
+    /**
+     * Returns the Product from the database matching the given PurchaseItem ID, otherwise null.
+     * PurchaseItem ID must not be null, otherwise it will throw IllegalArgumentException.
+     * @param purchase_item The PurchaseItem ID to get the Product from.
+     * @return The Product from the database matching the given PurchaseItem ID, otherwise null.
+     * @throws IllegalArgumentException If PurchaseItem ID is null.
+     * @throws DAOException If something fails at database level.
+     */          
     public Product findProduct(PurchaseItem purchase_item) throws IllegalArgumentException, DAOException;
     
+    /**
+     * Returns a list of all PurchaseItem from a given OrderPurchase from the database ordered by PurchaseItem ID. The OrderPurchase ID must not be null
+     * otherwise it will throw IllegalArgumentException. The list is never null and
+     * is empty when the database does not contain any PurchaseItem matching OrderPurchase.
+     * @param order_purchase The OrderPurchase to be searched for.
+     * @return A list of all PurchaseItem matching OrderPurchase from the database ordered by PurchaseItem ID.
+     * @throws IllegalArgumentException If the OrderPurchase ID is null.
+     * @throws DAOException If something fails at database level.
+     */
     public List<PurchaseItem> list(OrderPurchase order_purchase) throws IllegalArgumentException, DAOException;
     
+    /**
+     * Create the given PurchaseItem for a given OrderPurchase in the database. The OrderPurchase ID must not be null,
+     * the Product ID must not be null and the PurchaseItem ID must be null,
+     * otherwise it will throw IllegalArgumentException. 
+     * After creating, the DAO will set the obtained ID in the given CompanyAddress.
+     * @param order_purchase The OrderPurchase to be assigned to the PurchaseItem.
+     * @param product The Product to be assigned to the PurchaseItem.
+     * @param purchase_item The PurchaseItem to be created.
+     * @throws IllegalArgumentException If the OrderPurchase ID is null.
+     * @throws IllegalArgumentException If the Product ID is null.
+     * @throws IllegalArgumentException If the PurchaseItem ID is not null.
+     * @throws DAOException If something fails at database level.
+     */    
     public void create(OrderPurchase order_purchase, Product product, PurchaseItem purchase_item) throws IllegalArgumentException, DAOException;
     
     public void update(PurchaseItem purchase_item) throws IllegalArgumentException, DAOException;
