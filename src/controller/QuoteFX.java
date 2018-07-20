e3bcf19630421730b9c6ed7a63a23659f7ee1b5f
carloosarias <carloosarias@gmail.com>
2018-07-20 11:28:31 -0500

    Commit

--- src/controller/QuoteFX.java
+++ src/controller/QuoteFX.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 
@@ -28,20 +29,28 @@ public class QuoteFX implements Initializable {
     @FXML
     private ComboBox<?> partrev_combo;
     @FXML
+    private ComboBox<?> status_combo;
+    @FXML
     private Button add_quote;
     @FXML
-    private TableView<?> approved_tableview;
+    private TableView<?> quote_tableview;
     @FXML
-    private TableView<?> rejected_tableview;
+    private TableColumn<?, ?> id_column;
     @FXML
-    private TableView<?> pending_tableview;
+    private TableColumn<?, ?> quotedate_column;
+    @FXML
+    private TableColumn<?, ?> contact_column;
+    @FXML
+    private TableColumn<?, ?> unitprice_column;
+    @FXML
+    private TableColumn<?, ?> comments_column;
+    @FXML
+    private TableColumn<?, ?> status_column;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
     
 }
