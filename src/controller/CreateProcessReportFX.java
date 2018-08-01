23a9a94fe9579335bddf876b1ab3f61f32f4560f
carloosarias <carloosarias@gmail.com>
2018-08-01 10:00:59 -0500

    Commit

--- src/controller/CreateProcessReportFX.java
+++ src/controller/CreateProcessReportFX.java
@@ -19,6 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -63,7 +64,7 @@ public class CreateProcessReportFX implements Initializable {
     @FXML
     private TextField voltage_field;
     @FXML
-    private Spinner<Integer> starthour_spinner =  new Spinner<Integer>(0, 23, 1);
+    private Spinner<Integer> starthour_spinner;
     @FXML
     private Spinner<Integer> startminute_spinner =  new Spinner<Integer>(0, 59, 1);
     @FXML
@@ -75,6 +76,17 @@ public class CreateProcessReportFX implements Initializable {
     @FXML
     private Button save_button;
     
+    private ProductPart partnumbercombo_selection;
+    private String partnumbercombo_text;
+    
+    private PartRevision revisioncombo_selection;
+    private String revisioncombo_text;
+    
+    private SpinnerValueFactory starthour_factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 0);
+    private SpinnerValueFactory startminute_factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0);
+    private SpinnerValueFactory timerhour_factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 0);
+    private SpinnerValueFactory timerminute_factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0);
+    
     private DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
 
     /**
@@ -82,6 +94,8 @@ public class CreateProcessReportFX implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        setSpinnerValues();
+        
         employee_combo.setItems(FXCollections.observableArrayList(msabase.getEmployeeDAO().find(MainApp.employee_id)));
         process_combo.setItems(FXCollections.observableArrayList(MainApp.process_list));
         tank_combo.setItems(FXCollections.observableArrayList(msabase.getContainerDAO().listType("Tanque")));
@@ -94,6 +108,52 @@ public class CreateProcessReportFX implements Initializable {
             container_combo.setItems(FXCollections.observableArrayList(msabase.getContainerDAO().listType(newValue)));
             container_combo.setDisable(container_combo.getItems().isEmpty());
         });
+        
+        partnumber_combo.setOnAction((ActionEvent) -> {
+            partnumbercombo_text = partnumber_combo.getEditor().textProperty().getValue();
+            partnumbercombo_selection = msabase.getProductPartDAO().find(partnumbercombo_text);
+            if(partnumbercombo_selection == null){
+                partnumber_combo.getEditor().selectAll();
+            }else{
+                updatePartrev_combo();
+                revision_combo.requestFocus();
+            }
+            ActionEvent.consume();
+        });
+        
+        revision_combo.setOnAction((ActionEvent) -> {
+            if(partnumbercombo_selection == null){
+                ActionEvent.consume();
+                return;
+            }
+            revisioncombo_text = revision_combo.getEditor().textProperty().getValue();
+            revisioncombo_selection = msabase.getPartRevisionDAO().find(partnumbercombo_selection, revisioncombo_text);
+            if(revisioncombo_selection == null){
+                revision_combo.getEditor().selectAll();
+            }
+            else{
+                process_combo.getSelectionModel().select(revisioncombo_selection.getFinal_process());
+                lotnumber_field.requestFocus();
+                ActionEvent.consume();
+            }            
+        });
+        
+    }
+    
+    public void setSpinnerValues(){
+        starthour_spinner.setValueFactory(starthour_factory);
+        startminute_spinner.setValueFactory(startminute_factory);
+        timerhour_spinner.setValueFactory(timerhour_factory);
+        timerminute_spinner.setValueFactory(timerminute_factory);
+    }
+    
+    public void updatePartrev_combo(){
+        try{
+            revision_combo.setItems(FXCollections.observableArrayList(msabase.getPartRevisionDAO().list(partnumbercombo_selection)));
+        } catch(Exception e) {
+            revision_combo.getItems().clear();
+        }
+        revision_combo.setDisable(revision_combo.getItems().isEmpty());
     }
     
 }
