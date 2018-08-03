2801c8bfe709d23f0944a8be3576c7e21e213ab8
carloosarias <carloosarias@gmail.com>
2018-08-03 15:23:44 -0500

    Commit

--- src/controller/TransactionHistoryFX.java
+++ src/controller/TransactionHistoryFX.java
@@ -8,10 +8,16 @@ package controller;
 import dao.JDBC.DAOFactory;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.HBox;
 import model.ProductPart;
 
@@ -24,19 +30,45 @@ public class TransactionHistoryFX implements Initializable {
 
     @FXML
     private HBox root_hbox;
-    
-    private DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
+    @FXML
+    private ComboBox<ProductPart> partnumber_combo;
+    @FXML
+    private DatePicker startdate_picker;
+    @FXML
+    private DatePicker enddate_picker;
 
+    private DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        List<ProductPart> part_list = msabase.getProductPartDAO().listActive(true);
+        partnumber_combo.setItems(FXCollections.observableList(msabase.getProductPartDAO().listActive(true)));
+        startdate_picker.setValue(LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonth(), 1));
+        enddate_picker.setValue(startdate_picker.getValue().plusMonths(1).minusDays(1));
         
-        msabase.getDepartLotDAO().listDateRange(msabase.getProductPartDAO().find(1), java.sql.Date.valueOf(LocalDate.MIN), java.sql.Date.valueOf(LocalDate.now()));
-        msabase.getProcessReportDAO().listDateRange(msabase.getProductPartDAO().find(1), java.sql.Date.valueOf(LocalDate.MIN), java.sql.Date.valueOf(LocalDate.now()));
-        msabase.getIncomingLotDAO().listDateRange(msabase.getProductPartDAO().find(1), java.sql.Date.valueOf(LocalDate.MIN), java.sql.Date.valueOf(LocalDate.now()));
-    }    
+        partnumber_combo.setOnAction((ActionEvent) -> {
+            updateList(partnumber_combo.getSelectionModel().getSelectedItem(), java.sql.Date.valueOf(startdate_picker.getValue()), java.sql.Date.valueOf(enddate_picker.getValue()));
+        });
+        
+        startdate_picker.setOnAction((ActionEvent) ->{
+            partnumber_combo.fireEvent(new ActionEvent());
+        });
+        
+        enddate_picker.setOnAction((ActionEvent) -> {
+            partnumber_combo.fireEvent(new ActionEvent());
+        });
+    }
+    
+    public void updateList(ProductPart product_part, Date start_date, Date end_date){
+        try{
+            msabase.getDepartLotDAO().listDateRange(product_part, start_date, end_date);
+            msabase.getProcessReportDAO().listDateRange(product_part, start_date, end_date);
+            msabase.getIncomingLotDAO().listDateRange(product_part, start_date, end_date);
+        }catch(Exception e){
+            System.out.println("test");
+        }
+    }
     
 }
