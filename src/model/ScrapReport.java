9e5df28623640a731dc324019b54948c423f71e1
carloosarias <carloosarias@gmail.com>
2018-08-13 08:59:37 -0500

    Commit

---
+++ src/model/ScrapReport.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pavilion Mini
+ */
+public class ScrapReport {
+    // Properties ---------------------------------------------------------------------------------
+    private Integer id;
+    private Date report_date;
+    private String lot_number;
+    private Integer quantity;
+    private String comments;
+    
+    // Getters/setters ----------------------------------------------------------------------------
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getLot_number() {
+        return lot_number;
+    }
+
+    public void setLot_number(String lot_number) {
+        this.lot_number = lot_number;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    
+    // Object overrides ---------------------------------------------------------------------------
+    
+    /**
+     * This should compare ScrapReport by ID only.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object other) {
+        return (other instanceof ScrapReport) && (id != null)
+            ? id.equals(((ScrapReport) other).id)
+            : (other == this);
+    }
+
+    /**
+     * ScrapReport with same ID should return same hashcode.
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return (id != null) 
+             ? (this.getClass().hashCode() + id.hashCode()) 
+             : super.hashCode();
+    }
+
+    /**
+     * Returns the String representation of this ScrapReport. Not required, it just pleases reading logs.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return String.format("%d",
+                id);
+    }
+}
