341e6f202172b7c60af57f8d3235bcc13f726219
carloosarias <carloosarias@gmail.com>
2018-08-17 11:40:12 -0500

    Commit

--- src/controller/TransactionHistoryFX.java
+++ src/controller/TransactionHistoryFX.java
@@ -161,6 +161,7 @@ public class TransactionHistoryFX implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         setDepartTableViewItems();
         setProcessReportTableViewItems();
         setIncomingTableViewItems();
