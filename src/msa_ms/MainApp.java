a2c7546a32f669155a7aa5019e59b0609a4d85c6
carloosarias <carloosarias@gmail.com>
2018-08-20 14:42:19 -0500

    Commit

--- src/msa_ms/MainApp.java
+++ src/msa_ms/MainApp.java
@@ -33,8 +33,8 @@ public class MainApp extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        File file = new File("src/template/Test.pdf");
-        Desktop.getDesktop().open(file);
+        //File file = new File("src/template/Test.pdf");
+        //Desktop.getDesktop().open(file);
         BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/fxml/LoginFX.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("MSA Manager");
