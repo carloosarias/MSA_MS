d02ab9f57a000de7d6c5b3c0f9b4ec056b7407f0
carloosarias <carloosarias@gmail.com>
2018-09-21 12:50:56 -0500

    Commit

--- src/controller/ProductPartFX.java
+++ src/controller/ProductPartFX.java
@@ -8,6 +8,7 @@ package controller;
 import dao.JDBC.DAOFactory;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.property.SimpleStringProperty;
@@ -22,6 +23,7 @@ import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import model.PartRevision;
 import model.ProductPart;
 
 /**
@@ -44,27 +46,27 @@ public class ProductPartFX implements Initializable {
     @FXML
     private TableColumn<ProductPart, String> productpartstatus_column;
     @FXML
-    private TableView<?> partrevision_tableview;
+    private TableView<PartRevision> partrevision_tableview;
     @FXML
-    private TableColumn<?, ?> partrevisionid_column;
+    private TableColumn<PartRevision, Integer> partrevisionid_column;
     @FXML
-    private TableColumn<?, ?> rev_column;
+    private TableColumn<PartRevision, String> rev_column;
     @FXML
-    private TableColumn<?, ?> revdate_column;
+    private TableColumn<PartRevision, Date> revdate_column;
     @FXML
-    private TableColumn<?, ?> basemetal_column;
+    private TableColumn<PartRevision, String> basemetal_column;
     @FXML
-    private TableColumn<?, ?> finalprocess_column;
+    private TableColumn<PartRevision, String> finalprocess_column;
     @FXML
-    private TableColumn<?, ?> specificationnumber_column;
+    private TableColumn<PartRevision, String> specificationnumber_column;
     @FXML
-    private TableColumn<?, ?> area_column;
+    private TableColumn<PartRevision, Double> area_column;
     @FXML
-    private TableColumn<?, ?> baseweight_column;
+    private TableColumn<PartRevision, Double> baseweight_column;
     @FXML
-    private TableColumn<?, ?> finalweight_column;
+    private TableColumn<PartRevision, Double> finalweight_column;
     @FXML
-    private TableColumn<?, ?> partrevisionstatus_column;
+    private TableColumn<PartRevision, String> partrevisionstatus_column;
     @FXML
     private Button addproductpart_button;
     @FXML
@@ -72,7 +74,7 @@ public class ProductPartFX implements Initializable {
     
     private Stage add_stage = new Stage();
     
-    private List<String> productpartstatus_items = Arrays.asList("Activo", "Inactivo");
+    private List<String> status_items = Arrays.asList("Activo", "Inactivo");
     private DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
     
     /**
@@ -80,15 +82,15 @@ public class ProductPartFX implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
         setProductPartTable();
+        setPartRevisionTable();
     }    
     
     public void setProductPartTable(){
         productpartid_column.setCellValueFactory(new PropertyValueFactory<>("id"));
         partnumber_column.setCellValueFactory(new PropertyValueFactory<>("quote_date"));
         description_column.setCellValueFactory(new PropertyValueFactory<>("description"));
-        productpartstatus_column.setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(productpartstatus_items)));
+        productpartstatus_column.setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(status_items)));
         productpartstatus_column.setOnEditCommit((TableColumn.CellEditEvent<ProductPart, String> t) -> {
             t.getTableView().getItems().get(t.getTablePosition().getRow()).setActive(getActive(t.getNewValue()));
             msabase.getProductPartDAO().update(t.getTableView().getItems().get(t.getTablePosition().getRow()));
@@ -96,9 +98,32 @@ public class ProductPartFX implements Initializable {
         });
     }
     
+    public void setPartRevisionTable(){
+        partrevisionid_column.setCellValueFactory(new PropertyValueFactory<>("id"));
+        rev_column.setCellValueFactory(new PropertyValueFactory<>("rev"));
+        revdate_column.setCellValueFactory(new PropertyValueFactory<>("rev_date"));
+        basemetal_column.setCellValueFactory(new PropertyValueFactory<>("base_metal"));
+        finalprocess_column.setCellValueFactory(new PropertyValueFactory<>("final_process"));
+        specificationnumber_column.setCellValueFactory(new PropertyValueFactory<>("specification_number"));
+        area_column.setCellValueFactory(new PropertyValueFactory<>("area"));
+        baseweight_column.setCellValueFactory(new PropertyValueFactory<>("base_weight"));
+        finalweight_column.setCellValueFactory(new PropertyValueFactory<>("final_weight"));
+        partrevisionstatus_column.setCellFactory(ComboBoxTableCell.forTableColumn(FXCollections.observableArrayList(status_items)));
+        partrevisionstatus_column.setOnEditCommit((TableColumn.CellEditEvent<PartRevision, String> t) -> {
+            t.getTableView().getItems().get(t.getTablePosition().getRow()).setActive(getActive(t.getNewValue()));
+            msabase.getPartRevisionDAO().update(t.getTableView().getItems().get(t.getTablePosition().getRow()));
+            setPartRevisionItems(productpart_tableview.getSelectionModel().getSelectedItem());
+        });
+    }
+    
     public void setProductPartItems(){
         productpart_tableview.setItems(FXCollections.observableArrayList(msabase.getProductPartDAO().list()));
     }
+    
+    public void setPartRevisionItems(ProductPart product_part){
+        partrevision_tableview.setItems(FXCollections.observableArrayList(msabase.getPartRevisionDAO().list(product_part)));
+    }
+    
     public boolean getActive(String productpart_status){
         if(productpart_status.equals("Activo")){
             return true;
