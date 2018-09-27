024581b89207b035da0d41620ef2d2e761e906fc
carloosarias <carloosarias@gmail.com>
2018-09-27 14:39:46 -0500

    Commit

--- src/controller/AddMetalFX.java
+++ src/controller/AddMetalFX.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+import dao.JDBC.DAOFactory;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -12,6 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import model.Metal;
 
 /**
  * FXML Controller class
@@ -28,13 +31,49 @@ public class AddMetalFX implements Initializable {
     private TextField density_field;
     @FXML
     private Button save_button;
+    
+    private DAOFactory msabase = DAOFactory.getInstance("msabase.jdbc");
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        save_button.setOnAction((ActionEvent) -> {
+            if(!testFields()){
+                return;
+            }
+            saveMetal();
+            Stage stage = (Stage) root_hbox.getScene().getWindow();
+            stage.close();
+        });
     }    
     
+    public boolean testFields(){
+        boolean b = true;
+        clearStyle();
+        if(metalname_field.getText().replace(" ", "").equals("")){
+            metalname_field.setStyle("-fx-background-color: lightpink;");
+            b = false;
+        }
+        try{
+            Double.parseDouble(density_field.getText());
+        } catch(Exception e){
+            density_field.setStyle("-fx-background-color: lightpink;");
+            b = false;
+        }
+        return b;
+    }
+    
+    public void clearStyle(){
+        metalname_field.setStyle(null);
+        density_field.setStyle(null);
+    }
+    
+    public void saveMetal(){
+        Metal item = new Metal();
+        item.setMetal_name(metalname_field.getText());
+        item.setDensity(Double.parseDouble(density_field.getText()));
+        msabase.getMetalDAO().create(item);
+    }
 }
