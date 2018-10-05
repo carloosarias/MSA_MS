ffc7a1e3ce0808acc4a0b36a99b077bc6207aa74
carloosarias <carloosarias@gmail.com>
2018-10-05 15:18:49 -0500

    Commit

--- src/controller/QuoteFX.java
+++ src/controller/QuoteFX.java
@@ -6,16 +6,21 @@
 package controller;
 
 import dao.JDBC.DAOFactory;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
@@ -25,11 +30,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.PartRevision;
 import model.ProductPart;
 import model.Quote;
 import model.QuoteItem;
+import model.Specification;
 
 
 
@@ -42,6 +50,8 @@ public class QuoteFX implements Initializable {
     
     @FXML
     private HBox root_hbox;
+    @FXML
+    private VBox filter_vbox;
     @FXML
     private ComboBox<ProductPart> part_combo;
     @FXML
@@ -81,9 +91,9 @@ public class QuoteFX implements Initializable {
     @FXML
     private TableColumn<QuoteItem, String> weight_column;
     @FXML
-    private TableColumn<QuoteItem, Double> estimatedprice_column;
+    private TableColumn<QuoteItem, String> estimatedprice_column;
     @FXML
-    private TextField specificationnumber_field;
+    private ComboBox<Specification> specification_combo;
     @FXML
     private TextField specificationprocess_field;
     @FXML
@@ -109,7 +119,7 @@ public class QuoteFX implements Initializable {
         status_combo.getSelectionModel().selectFirst();
         
         setQuoteTableView();
-        
+        setQuoteItemTableView();
         part_combo.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends ProductPart> observable, ProductPart oldValue, ProductPart newValue) -> {
             setPartRevisionItems(newValue);
             partrev_combo.setDisable(partrev_combo.getItems().isEmpty());
@@ -117,6 +127,7 @@ public class QuoteFX implements Initializable {
         
         partrev_combo.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends PartRevision> observable, PartRevision oldValue, PartRevision newValue) -> {
            setQuoteItems();
+           add_button.setDisable(partrev_combo.getSelectionModel().isEmpty());
         });
         
         status_combo.setOnAction((ActionEvent) -> {
@@ -127,10 +138,62 @@ public class QuoteFX implements Initializable {
             setQuoteItems();
             status_combo.setDisable(!status_checkbox.isSelected());
         });
+        
+        quote_tableview.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends Quote> observable, Quote oldValue, Quote newValue) -> {
+            setQuoteItemItems();
+            setQuoteFields();
+        });
+        
+        add_button.setOnAction((ActionEvent) -> {
+            filter_vbox.setDisable(true);
+            showAddStage();
+            setQuoteItems();
+        });
+    }
+    
+    public void showAddStage(){
+        try {
+            add_stage = new Stage();
+            add_stage.initOwner((Stage) root_hbox.getScene().getWindow());
+            HBox root = (HBox) FXMLLoader.load(getClass().getResource("/fxml/CreateQuoteFX.fxml"));
+            Scene scene = new Scene(root);
+            
+            add_stage.setTitle("Nueva Cotización");
+            add_stage.setResizable(false);
+            add_stage.initStyle(StageStyle.UTILITY);
+            add_stage.setScene(scene);
+            add_stage.showAndWait();
+            filter_vbox.setDisable(false);
+        } catch (IOException ex) {
+            Logger.getLogger(QuoteFX.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void setPartRevisionItems(ProductPart product_part){
         partrev_combo.setItems(FXCollections.observableArrayList(msabase.getPartRevisionDAO().list(product_part)));
+    }
+    
+    public void setQuoteFields(){
+        if(quote_tableview.getSelectionModel().getSelectedItem() == null){
+            specification_combo.getItems().clear();
+            specificationprocess_field.clear();
+            revisionarea_field.clear();
+            quotemargin_field.clear();
+            estimatedtotal_field.clear();
+        }else{
+            specification_combo.setItems(FXCollections.observableArrayList(
+                    msabase.getPartRevisionDAO().findSpecification(
+                            msabase.getQuoteDAO().findPartRevision(
+                                    quote_tableview.getSelectionModel().getSelectedItem()
+                            )
+                    )
+            ));
+            specification_combo.getSelectionModel().selectFirst();
+            specificationprocess_field.setText(specification_combo.getSelectionModel().getSelectedItem().getProcess());
+            revisionarea_field.setText(""+msabase.getQuoteDAO().findPartRevision(quote_tableview.getSelectionModel().getSelectedItem()).getArea());
+            quotemargin_field.setText(""+quote_tableview.getSelectionModel().getSelectedItem().getMargin());
+            estimatedtotal_field.setText(""+quote_tableview.getSelectionModel().getSelectedItem().getEstimated_total());
+        }
     }
     
     public void setQuoteItems(){
@@ -143,6 +206,30 @@ public class QuoteFX implements Initializable {
                 quote_tableview.setItems(FXCollections.observableArrayList(msabase.getQuoteDAO().list(partrev_combo.getSelectionModel().getSelectedItem())));
             }
         }
+    }
+    
+    public void setQuoteItemItems(){
+        if(quote_tableview.getSelectionModel().getSelectedItem() == null){
+            quoteitem_tableview.getItems().clear();
+        }else{
+            quoteitem_tableview.setItems(FXCollections.observableArrayList(msabase.getQuoteItemDAO().list(quote_tableview.getSelectionModel().getSelectedItem())));
+        }
+    }
+    
+    public void setQuoteItemTableView(){
+        listnumber_column.setCellValueFactory(c -> new SimpleStringProperty(""+quoteitem_tableview.getItems().indexOf(c.getValue())+1));
+        metal_name_column.setCellValueFactory(c -> new SimpleStringProperty(
+                msabase.getSpecificationItemDAO().findMetal(
+                    msabase.getQuoteItemDAO().findSpecificationItem(
+                                c.getValue()
+                    )
+                ).getMetal_name()));
+        density_column.setCellValueFactory(c -> new SimpleStringProperty(""+c.getValue().getDensity()));
+        unitprice_column.setCellValueFactory(new PropertyValueFactory<>("unit_price"));
+        maximumthickness_column.setCellValueFactory(c -> new SimpleStringProperty(""+c.getValue().getThickness()));
+        volume_column.setCellValueFactory(c -> new SimpleStringProperty(""+c.getValue().getVolume()));
+        weight_column.setCellValueFactory(c -> new SimpleStringProperty(""+c.getValue().getWeight()));
+        estimatedprice_column.setCellValueFactory(c -> new SimpleStringProperty(""+c.getValue().getEstimatedPrice()));
     }
     
     public void setQuoteTableView(){
